c30c78a21175fd2abb6ddcdff927dc6a2845bccd
friverom <federico.rivero.m@gmail.com>
2023-06-27 20:45:08 -0400

    Added AnalogInout class to read sensors

--- src/rpi_points/AnalogInput.java
+++ src/rpi_points/AnalogInput.java
@@ -12,12 +12,17 @@ public class AnalogInput {
     
     private final int analog_input_num;
     private final AnalogInputType type;
+    //Variables of sensing range
     private final double sensor_min;
     private final double sensor_max;
     private final double sensor_span;
-    private double span;
-    private double factor=1.0;
-    private double zero=0;
+    //Variables of sensing transmitter
+    private double tx_min;
+    private double tx_span;
+    //Variables of ADC conversion
+    private double adc_min;
+    private double adc_span;
+    
     private RPI_IO rpio;
     
     public AnalogInput(int num, AnalogInputType type, double min, double max, RPI_IO rpio){
@@ -33,43 +38,48 @@ public class AnalogInput {
     private void adjust_input(){
         switch(this.type){
             case TYPE_4_20mA:
-                this.factor=16/(4096-820);
-                this.span=4096-820;
-                this.zero=4.0;
+                this.tx_min=4;
+                this.tx_span=16;
+                this.adc_min=820;
+                this.adc_span=4096-820;
                 break;
                 
             case TYPE_0_20mA:
-                this.factor=20/4096;
-                this.span=4096;
-                this.zero=0.0;
+                this.tx_min=0;
+                this.tx_span=20;
+                this.adc_min=0;
+                this.adc_span=4096;
                 break;
                 
             case TYPE_0_5V:
-                this.factor=5/4096;
-                this.span=4096;
-                this.zero=0.0;
+                this.tx_min=0;
+                this.tx_span=5;
+                this.adc_min=0;
+                this.adc_span=4096;
                 break;
                 
             case TYPE_0_10V:
-                this.factor=10/4096;
-                this.span=4096;
-                this.zero=0.0;
+                this.tx_min=0;
+                this.tx_span=10;
+                this.adc_min=0;
+                this.adc_span=4096;
                 break;
                 
             default:
-                this.factor=1.0;
-                this.span=4096;
-                this.zero=0.0;
+                this.tx_min=0;
+                this.tx_span=1;
+                this.adc_min=0;
+                this.adc_span=4096;
         }
     }
     
     public double analog_read(){
         //Get analog reading and convert to sensor type
         int read=this.rpio.getChannel(this.analog_input_num);
-        double value=read*this.factor+this.zero;
+        double value=((read-this.adc_min)/this.adc_span)*this.tx_span+this.tx_min;
         
         //Convert reading to sensor value
-        double sensor_value=(value/(this.factor*this.span))*this.sensor_span+this.sensor_min;
+        double sensor_value=((value-this.tx_min)/this.tx_span)*this.sensor_span+this.sensor_min;
         
         return sensor_value;
     }
